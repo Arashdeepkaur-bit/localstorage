9d2036d36bd941709963ee238a7b614e4257ad5b
Arash <sainiarash555@gmail.com>
2024-11-29 10:46:50 +0530

    ejs

--- index.js
+++ index.js
@@ -1,38 +1,3 @@
-// const path = require('path');
-// const express = require('express');
-// const ejs = require('ejs');
-
-// const app = express();
-// const bodyParser = require('body-parser');
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-
-// console.log(__dirname);
-
-// app.set('views', path.join(__dirname, 'views'));
-
-// app.set('view engine', 'ejs');
-
-
-// app.get('/create', (req, res) => {
-
-//     res.render("create");
-
-// });
-
-// app.get('/show', (req, res) => {
-//     res.render("show");
-// });
-// app.get('/delete', (req, res) => {
-//     res.render("delete");
-// });
-// app.get('/update', (req, res) => {
-//     res.render("update");
-// });
-
-// app.listen(1111, () => {
-//     console.log('Server is running at port 8000');
-// });
 const express = require('express');
 const path = require('path');
 const app = express();
